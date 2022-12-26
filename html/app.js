411df1ac8b8d32a5fa70abfeaf28a2e06fee07eb
Okan Deniz <81102539+odgtr@users.noreply.github.com>
2022-12-26 18:10:45 +0300

    Delete html directory

--- html/app.js
+++
@@ -1,9 +0,0 @@
-// Defining the button
-const button = document.getElementById('buttonlogin');
-
-// When the user clicks on the button
-button.addEventListener('click', function handleClick() {
-    setTimeout(function(){
-        window.location.href = "/login.html";
-    }, 100);
-});
